1f4416c8ffb27fdeaf064732b481caf894b78bfe
Winston Teo <pair+winston@newcontext.com>
2012-07-10 16:40:36 +0800

    Update .pairs files to be NewContext specific.

--- attributes/git_pairs.rb
+++ attributes/git_pairs.rb
@@ -1,15 +1,14 @@
-# Sort alphabetically by first initial
+node.default["git_pairs_domain"]  = "newcontext.com"
+
 node.default["git_pairs_authors"] = [
-  {"initials" => "aml", "name" => "Alex Lobascio", "shortname" => "alobascio"},
-  {"initials" => "bc", "name" => "Brian Cunnie", "shortname" => "cunnie"},
-  {"initials" => "cd", "name" => "Colin Deeb", "shortname" => "deeb"},
-  {"initials" => "dr", "name" => "Doc Ritezel", "shortname" => "doc"},
-  {"initials" => "jb", "name" => "Jonathan Berger", "shortname" => "jonathanpberger"},
-  {"initials" => "jd", "name" => "Jeff Dean", "shortname" => "jdean"},
-  {"initials" => "lk", "name" => "Laurence Koret", "shortname" => "lkoret"},
-  {"initials" => "mk", "name" => "Matthew Kocher", "shortname" => "mkocher"},
-  {"initials" => "rh", "name" => "Rob Head", "shortname" => "rhead"},
-  {"initials" => "rk", "name" => "Reed Kennedy", "shortname" => "reed"},
-  {"initials" => "sb", "name" => "Sean Beckett", "shortname" => "sean"},
-  {"initials" => "tlp", "name" => "Tonya Podkuiko", "shortname" => "tpodkuiko"}
+  {"initials" => "as" , "name" => "Abhaya Shenoy"},
+  {"initials" => "ccm", "name" => "Carl Coryell-Martin"},
+  {"initials" => "dg" , "name" => "Diego Garcia"},
+  {"initials" => "mk" , "name" => "Mohan Krishnan"},
+  {"initials" => "mm" , "name" => "Mike Mazur"},
+  {"initials" => "tm" , "name" => "Tomek Maskowski"},
+  {"initials" => "ts" , "name" => "Tommy Sullivan"},
+  {"initials" => "wt" , "name" => "Winston Teo"},
+  {"initials" => "wei", "name" => "Wei Lu"},
+  {"initials" => "zi" , "name" => "Zi"}
 ]
