c25aa34da79964e3971377ab517cdb0afd832a53
Stanley Kwok <skwoka@gmail.com>
2012-12-03 22:39:14 -0800

    Use dmg_package resource

--- recipes/quicksilver.rb
+++ recipes/quicksilver.rb
@@ -1,11 +1,6 @@
-app="Quicksilver"
-
-pivotal_workstation_package app do
+dmg_package "Quicksilver" do
   source "http://cloud.github.com/downloads/quicksilver/Quicksilver/Quicksilver%20B70.dmg"
   action :install
   checksum "7df0f97ed008f626516c6478d997c02e"
 end
 
-execute "chown /Applications/#{app}.app" do
-  command "chown -R #{WS_USER} '/Applications/#{app}.app'"
-end
